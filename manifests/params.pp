32750c65c6ab4ce4f65c475c85b82ee0050e254b
root <root@puppetmaster.amg>
2017-10-17 15:56:38 -0500

    Initial commit.

---
+++ manifests/params.pp
@@ -0,0 +1,16 @@
+class ntp::params {
+  $package_name = 'ntp'
+  $pacakge_ensure = 'present'
+  $config_name = 'ntp.conf'
+  $config_file_mode = '0664'
+  $servers = ['time.seagate.com', 'time.intwideip.seagate.com']
+  $service_ensure = 'running'
+  $service_enable = true
+  $service_hasrestart = true
+  $service_hasstatus = true
+
+  $service_name = $facts['os']['family'] ? {
+    'Debian' => 'ntp',
+    default  => 'ntpd',
+  }
+}
