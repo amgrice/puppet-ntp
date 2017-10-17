d7ca579528cabf708ecd8907a158dbb625d3bf8a
root <root@puppetmaster.amg>
2017-10-17 16:13:50 -0500

    Modified params.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,13 @@
 class ntp::params {
-  $package_name = 'ntp'
-  $pacakge_ensure = 'present'
-  $config_name = 'ntp.conf'
-  $config_file_mode = '0664'
-  $servers = ['time.seagate.com', 'time.intwideip.seagate.com']
-  $service_ensure = 'running'
-  $service_enable = true
+  $package_name       = 'ntp'
+  $pacakge_ensure     = 'present'
+  $config_name        = 'ntp.conf'
+  $config_file_mode   = '0664'
+  $servers            = ['time.seagate.com', 'time.intwideip.seagate.com']
+  $service_ensure     = 'running'
+  $service_enable     = true
   $service_hasrestart = true
-  $service_hasstatus = true
+  $service_hasstatus  = true
 
   $service_name = $facts['os']['family'] ? {
     'Debian' => 'ntp',
